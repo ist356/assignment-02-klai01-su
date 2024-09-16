268b610667ca60d6344b88312deb5996c6a13666
klai01-su <klai01@syr.edu>
2024-09-16 00:01:20 -0400

    finished assignment 2

--- code/packaging.py
+++ code/packaging.py
@@ -18,8 +18,17 @@ def parse_packaging(packaging_data: str) -> list[dict]:
     input: "20 pieces in 1 pack / 10 packs in 1 carton / 4 cartons in 1 box"
     output: [{ 'pieces' : 20}, {'packs' : 10}, {'carton' : 4}, {'box' : 1}]
     '''
-    pass # TODO: Replace this line and write code
 
+    package = []
+    
+    for data in packaging_data.split('/'):
+        item = data.split(" in ")[0]
+        quantity = int(item.split()[0])
+        item = item.split()[1].strip()
+        package.append({item: quantity})
+
+    return package
+    
 
 def calc_total_units(package: list[dict]) -> int:
     '''
@@ -33,7 +42,15 @@ def calc_total_units(package: list[dict]) -> int:
     input: [{ 'pieces' : 20}, {'packs' : 10}, {'carton' : 4}, {'box' : 1}]
     output: 800 (e.g. 20*10*4*1)
     '''
-    pass # TODO: Replace this line and write code
+
+    total_units = 1
+    
+    for item in package:
+        quantity = list(item.values())[0]
+        total_units *= quantity
+    
+    return total_units
+    
 
 
 def get_unit(package: list[dict]) -> str:
@@ -49,7 +66,9 @@ def get_unit(package: list[dict]) -> str:
     output: pieces
 
     '''
-    pass # TODO: Replace this line and write code
+
+    return list(package[0].keys())[0]
+
 
 # This will only run from here, not when imported
 # # Use this for testing / debugging cases with the debugger
--- reflection.md
+++ reflection.md
@@ -1,7 +1,7 @@
 # Reflection
 
-Student Name:  name
-Sudent Email:  email
+Student Name:  Kavias Lai
+Sudent Email:  klai01@syr.edu
 
 ## Instructions
 
@@ -22,4 +22,4 @@ Examples:
 **Best Reflection** "I learned when to use while vs for loops. While loops are for sentiel-controlled values (waiting for a condition to occur), vs for loops are for iterating over collections of fixed values."
 
 `--- Reflection Below This Line ---`
-
+I learned how to obtain data from a text file. When tackling the "parse_packaging" function, I learned how to obtain certain parts of data by using split() and then cleaning the data with strip() so that i can be usable for the overall program.
